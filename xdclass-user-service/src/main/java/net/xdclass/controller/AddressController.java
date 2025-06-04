e4a6d782621a1c5446b88a2c453ab33d674abf9c
sunshine <sunshine3468@163.com>
2025-06-04 17:24:38 +0800

    统一枚举类和响应协议

--- xdclass-user-service/src/main/java/net/xdclass/controller/AddressController.java
+++ xdclass-user-service/src/main/java/net/xdclass/controller/AddressController.java
@@ -4,7 +4,9 @@ package net.xdclass.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import net.xdclass.model.AddressDO;
 import net.xdclass.service.AddressService;
+import net.xdclass.util.JsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,7 +33,8 @@ public class AddressController {
     public Object detail(@ApiParam(value = "地址id",required = true)
                         @PathVariable("address_id") long addressId){
 
-        return  addressService.detail(addressId);
+       AddressDO addressDO=addressService.detail(addressId);
+       return JsonData.buildSuccess(addressDO);
     }
 }
 
